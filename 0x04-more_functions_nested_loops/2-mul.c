4d89b70dd7246393cad625340cb1406396f8bfc0
Imanuelle-glitch <chidiaemeruemfavour2017@gmail.com>
2023-09-15 17:39:33 -0700

    update readme

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: character to multiply
+ * @b: character to multiply with
+ *
+ * Return: multiplication result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-a function that checks for digit
+function that multiplies two integers
