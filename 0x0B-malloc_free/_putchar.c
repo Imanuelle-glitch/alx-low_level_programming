fc347d26b3323d6b015a8a4725b7d401b9de8bee
Imanuelle-glitch <chidiaemeruemfavour2017@gmail.com>
2023-10-09 09:54:03 -0700

    update readme

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: 0 on success, -1 on error, and error is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
