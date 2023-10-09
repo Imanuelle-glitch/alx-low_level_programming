e86409c7f99357a1a07256890ecb480be3d47010
Imanuelle-glitch <chidiaemeruemfavour2017@gmail.com>
2023-10-08 20:10:40 -0700

    update readme

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERR_MSG "Error"
+
+/**
+ * is_digit - checks if a string contains a non-digit character
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwose
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+}
